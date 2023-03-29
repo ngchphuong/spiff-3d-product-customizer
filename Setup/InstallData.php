51b610b097c1ee89343f7e33900575ca86a48707
Thoang Le <thoang@lotusoutsourcing.com>
2023-03-29 09:03:06 +0700

    Init

---
+++ Setup/InstallData.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Spiff\Personalize\Setup;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+* @codeCoverageIgnore
+*/
+class InstallData implements InstallDataInterface
+{
+    /**
+     * Eav setup factory
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @inheritdoc
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create();
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'enable_spiff',
+            [
+                'group' => 'General',
+                'type' => 'int',
+                'label' => 'Enable Spiff',
+                'input' => 'boolean',
+                'source' =>  \Magento\Eav\Model\Entity\Attribute\Source\Boolean::class,
+                'frontend' => '',
+                'backend' => '',
+                'required' => false,
+                'sort_order' => 50,
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'is_used_in_grid' => false,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'visible' => true,
+                'is_html_allowed_on_front' => true,
+                'visible_on_front' => true
+            ]
+        );
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'spiff_integration_product_id',
+            [
+                'group' => 'General',
+                'type' => 'varchar',
+                'label' => 'Spiff Integration Product ID',
+                'input' => 'text',
+                'source' =>  \Magento\Eav\Model\Entity\Attribute\Source\Boolean::class,
+                'frontend' => '',
+                'backend' => '',
+                'required' => false,
+                'sort_order' => 60,
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'is_used_in_grid' => false,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'visible' => true,
+                'is_html_allowed_on_front' => true,
+                'visible_on_front' => true
+            ]
+        );
+    }
+}
